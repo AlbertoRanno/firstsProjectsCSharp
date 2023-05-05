16e693385733c462da0c18504915749e79145ba2
AlbertoRanno <albertoranno@hotmail.com>
2023-05-05 18:00:07 -0300

    MissingPets - Add a new animal friend to the ourAnimals array

--- MissingPets/Program.cs
+++ MissingPets/Program.cs
@@ -28,7 +28,7 @@ La matriz puede ser accedida y modificada utilizando índices de fila y columna 
 for (int i = 0; i < maxPets; i++) //Armo la matriz:
 { // cada fila (i) de la matriz, será una mascota, y cada columna, será una característica de la misma:
   //animalSpecies | animalID | animalAge | animalPhysicalDescription | animalPersonalityDescription | animalNickname:
-  switch (i) 
+  switch (i)
   {
     case 0:
       animalSpecies = "dog";
@@ -106,7 +106,7 @@ do //el usuario ve al menos una vez el menú
     menuSelection = readResult.ToLower();
   }
 
-   switch (menuSelection)
+  switch (menuSelection)
   {
     case "1":
       //List all of our current pet information
@@ -124,13 +124,217 @@ do //el usuario ve al menos una vez el menú
       Console.WriteLine("Press the Enter key to continue");
       readResult = Console.ReadLine();
       break;
-    /*La razón por la que no se usa un bucle foreach en esta situación es porque ourAnimals es una matriz multidimensional. Dado que ourAnimals es una matriz de cadenas multidimensional, cada elemento que ourAnimals contiene es un elemento independiente de la cadena de tipo. Si usó un bucle foreach para recorrer en iteración ourAnimals, foreach reconocería cada cadena como un elemento independiente en una lista de 48 elementos de cadena (8 x 6 = 48). La instrucción foreach no procesaría las dos dimensiones de la matriz por separado. En otras palabras, un bucle foreach no reconocerá 8 filas de elementos de cadena, donde cada fila contiene una columna de 6 elementos. Como quiere trabajar con los animales de uno en uno y procesar las seis características de cada animal durante una sola iteración, la instrucción foreach no es la más adecuada.*/
+    /*
+    Cuando se procesa el contenido de una matriz multidimensional, un desarrollador quiere a menudo recorrer en iteraciòn las dimensiones de una matriz por separado. Y la instrucciòn FOR lo permite de una manera mucho más simple
+    
+    La razón por la que no se usa un bucle foreach en esta situación es porque ourAnimals es una matriz multidimensional. Dado que ourAnimals es una matriz de cadenas multidimensional, cada elemento que ourAnimals contiene es un elemento independiente de la cadena de tipo. Si usó un bucle foreach para recorrer en iteración ourAnimals, foreach reconocería cada cadena como un elemento independiente en una lista de 48 elementos de cadena (8 x 6 = 48). La instrucción foreach no procesaría las dos dimensiones de la matriz por separado. En otras palabras, un bucle foreach no reconocerá 8 filas de elementos de cadena, donde cada fila contiene una columna de 6 elementos. Como quiere trabajar con los animales de uno en uno y procesar las seis características de cada animal durante una sola iteración, la instrucción foreach no es la más adecuada.*/
     case "2":
       //Add a new animal friend to the ourAnimals array
-      Console.WriteLine("this app feature is coming soon - please check back to see progress.");
-      Console.WriteLine("Press the Enter key to continue");
-      readResult = Console.ReadLine();
+      string anotherPet = "y";
+      int petCount = 0;
+
+      //cuento los animales que ya tenemos
+      for (int i = 0; i < maxPets; i++)
+      {
+        if (ourAnimals[i, 0] != "ID #: ")
+        {
+          petCount++;
+        }
+      }
+
+      if (petCount < maxPets)
+      {
+        Console.WriteLine($"We currently have {petCount} pets that need homes. We can manage {(maxPets - petCount)} more.");
+      }
+
+      bool validEntry = false;
+
+      // defino la especie:
+      do
+      {
+        Console.WriteLine("\n\rEnter 'dog' or 'cat' to begin a new entry");
+        //El "\r"  significa "retorno de carro" (carriage return) se usa para mover el cursor al principio de la siguiente línea.
+        readResult = Console.ReadLine();
+        if (readResult != null)
+        {
+          animalSpecies = readResult.ToLower();
+          if (animalSpecies != "dog" && animalSpecies != "cat")
+          {
+            Console.WriteLine($"You enteder: {animalSpecies}");
+            validEntry = false;
+          }
+          else
+          {
+            validEntry = true;
+          }
+        }
+      } while (validEntry == false);
+
+      //desde la posicion 0, avanzo 1 caracter, y corto el resto. Luego le sumo el nro siguiente a petCount, pasado a string  
+      animalID = animalSpecies.Substring(0, 1) + (petCount + 1).ToString();
+
+      // defino la edad:
+      do
+      {
+        int petAge;
+        Console.WriteLine("Enter the pet's age or enter ? if unknown");
+        readResult = Console.ReadLine();
+
+        if (readResult != null)
+        {
+          animalAge = readResult.ToLower();
+          if (animalAge != "?")
+          {
+            validEntry = int.TryParse(animalAge, out petAge);
+            /*
+            La función "int.TryParse()" se utiliza para intentar analizar una cadena (string) y convertirla en un valor entero (int).
+ "animalAge" es una cadena que contiene la edad de una mascota. La variable "petAge" es una variable de tipo entero (int) que se utiliza para almacenar la edad de la mascota después de que se convierte de una cadena a un valor entero.
+
+La función "int.TryParse()" tiene dos parámetros:
+
+El primer parámetro es la cadena (string) que se va a analizar y convertir a un valor entero.
+El segundo parámetro es una variable de salida (out) que almacenará el valor entero resultante después del análisis de la cadena.
+
+La función "int.TryParse()" devuelve un valor booleano que indica si el análisis y la conversión de la cadena fueron exitosos o no. Si la función devuelve "true", significa que la cadena se analizó y se convirtió correctamente a un valor entero, y el valor entero se almacenó en la variable "petAge". Si la función devuelve "false", significa que el análisis y la conversión de la cadena no fueron exitosos, y la variable "petAge" tendrá un valor predeterminado (en este caso, el valor predeterminado de una variable int es 0).
+            */
+          }
+        }
+      } while (validEntry == false);
+
+      do
+      {
+        Console.WriteLine("Enter a physical description of the pet (size, color, gender, weight, housebroken)");
+        readResult = Console.ReadLine();
+        if (readResult != null)
+        {
+          animalPhysicalDescription = readResult.ToLower();
+          if (animalPhysicalDescription == "")
+          {
+            animalPhysicalDescription = "tbd"; //significa "por determinar" ( to be determined )
+          }
+        }
+      } while (animalPhysicalDescription == "");
+
+      do
+      {
+        Console.WriteLine("Enter a description of the pet's personality (likes or dislikes, tricks, energy level)");
+        readResult = Console.ReadLine();
+        if (readResult != null)
+        {
+          animalPersonalityDescription = readResult.ToLower();
+          if (animalPersonalityDescription == "")
+          {
+            animalPersonalityDescription = "tbd";
+          }
+        }
+      } while (animalPersonalityDescription == "");
+
+      do
+      {
+        Console.WriteLine("Enter a nickname for the pet");
+        readResult = Console.ReadLine();
+        if (readResult != null)
+        {
+          animalNickname = readResult.ToLower();
+          if (animalNickname == "")
+          {
+            animalNickname = "tbd";
+          }
+        }
+      } while (animalNickname == "");
+
+      /* store the pet information in the ourAnimals array (zero based)
+      Al comienzo, guardé en las primeras filas de la matriz, los datos precargados en cada columna, de cada fila.
+      Ahora, cargaré en las filas restantes, dado a que petCount iniciará en "4", y se irá incrementando al cargar más mascotas.
+      Recordar que las primeras filas que se cargaron fueron de la fila 0, a la fila 3 inclusive (4 mascotas)
+      */
+      ourAnimals[petCount, 0] = "ID #: " + animalID;
+      ourAnimals[petCount, 1] = "Species: " + animalSpecies;
+      ourAnimals[petCount, 2] = "Age: " + animalAge;
+      ourAnimals[petCount, 3] = "Nickname: " + animalNickname;
+      ourAnimals[petCount, 4] = "Physical description: " + animalPhysicalDescription;
+      ourAnimals[petCount, 5] = "Personality: " + animalPersonalityDescription;
+
+      while (anotherPet == "y" && petCount < maxPets)
+      {
+        petCount++;
+        if (petCount < maxPets)
+        {
+          Console.WriteLine("Do you want to enter info for another pet (y/n)");
+          do
+          {
+            readResult = Console.ReadLine();
+            if (readResult != null)
+            {
+              anotherPet = readResult.ToLower();
+            }
+          } while (anotherPet != "y" && anotherPet != "n");
+        }
+      }
+
+
+      if (petCount >= maxPets)
+      {
+        Console.WriteLine("We have reached our limit on the number of pets that we can manage.");
+        Console.WriteLine("Press the Enter key to continue");
+        readResult = Console.ReadLine();
+      }
       break;
+
+    /* Comentarios son el análisis del código
+    en este caso, la lógica de código se basa en la relación entre petCount y maxPets. Sabe que a maxPets se le asigna un valor de 8, pero ¿qué hay de petCount? Los siguientes elementos ayudan a evaluar la lógica que ha implementado:
+
+Sabe que petCount es 4 cuando escribe la primera iteración del bucle while.
+
+Sabe que se incrementa petCount cada vez que el bucle while recorre en iteración.
+
+Sabe que el valor asignado a petCount y la forma en que se incrementa petCount afecta a cómo se almacenan los datos en la matriz ourAnimals. Los siguientes elementos explican la relación entre petCount y los datos almacenados en ourAnimals:
+
+La aplicación agrega cuatro mascotas a la matriz ourAnimals cuando crea los datos de ejemplo.
+La aplicación almacena nuevos datos en la matriz ourAnimals cuando el valor de petCount es 4. Esto no es un error. El código tiene sentido cuando se recuerda que los elementos de la matriz están basados en cero. Por ejemplo, ourAnimals[0,0] contiene el identificador de mascota para el animal 1, y ourAnimals[3,0] contiene el identificador de mascota para el animal 4. Por lo tanto, cuando petCount es 4, está almacenando datos para la quinta mascota.
+La aplicación almacenará los datos de mascotas en la matriz antes de incrementar petCount.
+La aplicación incrementa petCount antes de solicitar al usuario que agregue otra mascota.
+Cuando la aplicación muestra la solicitud ¿Desea especificar información sobre otra mascota? (s/n) por primera vez, petCount ya está establecido en 5.
+Si el usuario escribe y en el primer mensaje ¿Desea especificar información sobre otra mascota? (y/n), sabe que:
+
+El bucle while (anotherPet == "y" && petCount < maxPets) recorrerá en iteración. Sabe que el bucle recorrerá en iteración debido a anotherPet == "y" y petCount < maxPets.
+El valor asignado a petCount se incrementará (cuando el bucle while recorra en iteración).
+El valor asignado a petCount será 6 (después de que el usuario escriba y por primera vez).
+Tenga en cuenta este análisis de la lógica de código mientras continúa probando la aplicación.
+
+Observe que el panel del terminal se actualiza con el mismo mensaje "¿otra mascota?", pero el código no muestra un petCount actualizado.
+
+El panel del terminal debería mostrar ahora la siguiente salida:
+
+We currently have 4 pets that need homes. We can manage 4 more.
+Do you want to enter info for another pet (y/n)
+y
+Do you want to enter info for another pet (y/n)
+
+En el símbolo del sistema del terminal, escriba y
+
+Al escribir y por segunda vez, petCount se incrementa en 7. Por lo tanto, petCount sigue siendo menor que maxPets.
+
+En el símbolo del sistema del terminal, escriba y
+
+Al escribir y por tercera vez, petCount se incrementa en 8. Por lo tanto, ahora petCount es igual a maxPets.
+
+Compruebe que el código sale del bucle while al escribir y por tercera vez.
+
+El panel del terminal debería mostrar ahora la siguiente salida:
+
+We currently have 4 pets that need homes. We can manage 4 more.
+Do you want to enter info for another pet (y/n)
+y
+Do you want to enter info for another pet (y/n)
+y
+Do you want to enter info for another pet (y/n)
+y
+We have reached our limit on the number of pets that we can manage.
+Press the Enter key to continue.
+
+    */
+
     case "3":
       //Ensure animal ages and physical descriptions are complete
       Console.WriteLine("Challenge Project - please check back soon to see progress.");
--- tipsSueltos.md
+++ tipsSueltos.md
@@ -16,3 +16,11 @@ Nótese que no hay versiones compuestas para los operadores binarios && y ||.
 **** 
 c = b++; // Se asigna a c el valor de b y luego se incrementa b
 c = ++b; // Se incrementa el valor de b y luego se asigna a c
+
+****
+En que ámbito defino las variables?
+Defina siempre el ámbito de las variables de la forma más estrecha posible. Esto permite una mejor administraciòn de los recursos y ayuda a minimizare el perfil de ataque de la aplicación.
+
+ El ámbito más grande le permitiría acceder a ella desde cualquier lugar de la aplicación. Por lo tanto, ¿por qué no definir el ámbito de las variables en el nivel de aplicación cuando cree que se podrían usar más adelante en la aplicación? El ámbito de las variables en un nivel superior al necesario puede provocar problemas. El ámbito con privilegios elevados aumenta los requisitos de recursos de la aplicación y puede exponer la aplicación a riesgos de seguridad innecesarios. A medida que las aplicaciones aumentan de tamaño y son más complejas, requieren más recursos. Los teléfonos y equipos asignan memoria para estos recursos cuando están en el ámbito. A medida que las aplicaciones pasan a ser más "reales", se vuelven más accesibles. Las aplicaciones suelen ser accesibles desde la nube u otras aplicaciones. Al agravar estos problemas, las aplicaciones se suelen dejar en ejecución cuando no se usan. Es importante mantener los requisitos de recursos de una aplicación bajo control y la superficie de seguridad lo más reducida posible. Aunque los sistemas operativos actuales realizan un gran trabajo de administración de recursos y de protección de aplicaciones, sigue siendo recomendable mantener las variables en el ámbito del nivel en el que realmente se necesitan. 
+
+****
