3bcdb7aa6885ec0ab33e37b85b7cfc9022aa4e3e
AlbertoRanno <albertoranno@hotmail.com>
2023-05-11 17:03:28 -0300

    Terminados los ejercicios del módulo 2 de Alumni

--- EjerciciosModulo2/Program.cs
+++ EjerciciosModulo2/Program.cs
@@ -177,51 +177,258 @@ Ingresar dos números y ofrecer al usuario un menú con las siguientes opciones:
 "1-suma 2-resta 3-producto 4-división"
 Luego, mostrar el resultado de la operación aritmética elegida"*/
 
-double numero1 = 0, numero2 = 0; //********DUDA!!!! POR QUÉ SI NO LOS INICIALIZO, DA ERROR ASIGNARLES EL VALOR CUANDO SON INGRESADOS??
+// double numero1 = 0, numero2 = 0; //********DUDA!!!! POR QUÉ SI NO LOS INICIALIZO, DA ERROR ASIGNARLES EL VALOR CUANDO SON INGRESADOS??
 
-string? readResult;
-string menuSelection = "";
+// string? readResult;
+// string menuSelection = "";
 
-Console.Clear();
-Console.WriteLine("Bienvenidos a la calculadora básica en C#\n Ingrese un número:");
-readResult = Console.ReadLine();
-if (readResult != null)
-{
-  numero1 = double.Parse(readResult);
-}
-Console.WriteLine("Ingrese otro número:");
-readResult = Console.ReadLine();
-if (readResult != null)
-{
-  numero2 = double.Parse(readResult);
-}
-Console.WriteLine($"Usted ingresó {numero1} y {numero2}.\n Elija una de las siguientes opciones: ");
-Console.WriteLine("\t Presione \"1\" - Para sumar los números ingresados");
-Console.WriteLine("\t Presione \"2\" - Para restar los números ingresados");
-Console.WriteLine("\t Presione \"3\" - Para obtener el producto entre los números ingresados");
-Console.WriteLine("\t Presione \"4\" - Para realizar la división entre los números ingresados");
+// Console.Clear();
+// Console.WriteLine("Bienvenidos a la calculadora básica en C#\n Ingrese un número:");
+// readResult = Console.ReadLine();
+// if (readResult != null)
+// {
+//   numero1 = double.Parse(readResult);
+// }
+// Console.WriteLine("Ingrese otro número:");
+// readResult = Console.ReadLine();
+// if (readResult != null)
+// {
+//   numero2 = double.Parse(readResult);
+// }
+// Console.WriteLine($"Usted ingresó {numero1} y {numero2}.\n Elija una de las siguientes opciones: ");
+// Console.WriteLine("\t Presione \"1\" - Para sumar los números ingresados");
+// Console.WriteLine("\t Presione \"2\" - Para restar los números ingresados");
+// Console.WriteLine("\t Presione \"3\" - Para obtener el producto entre los números ingresados");
+// Console.WriteLine("\t Presione \"4\" - Para realizar la división entre los números ingresados");
 
-readResult = Console.ReadLine();
-if (readResult != null)
+// readResult = Console.ReadLine();
+// if (readResult != null)
+// {
+//   menuSelection = readResult.ToLower();
+// }
+
+// switch (int.Parse(menuSelection))
+// {
+//   case 1:
+//     Console.WriteLine($"La suma de {numero1} y {numero2} es igual a {numero1 + numero2} ");
+//     break;
+//   case 2:
+//     Console.WriteLine($"La resta de {numero1} y {numero2} es igual a {numero1 - numero2} ");
+//     break;
+//   case 3:
+//     Console.WriteLine($"El producto entre {numero1} y {numero2} es igual a {numero1 * numero2} ");
+//     break;
+//   case 4:
+//     Console.WriteLine($"La división entre {numero1} y {numero2} es igual a {numero1 / numero2} ");
+//     break;
+//   default:
+//     Console.WriteLine("No es una operación válida");
+//     break;
+// }
+
+
+
+
+/*************************************************************** Ejemplos ERRORES EN CICLOS WHILE*/
+// byte numero = 1;
+
+// while (numero <= 100)
+// {
+//   Console.WriteLine(numero);
+//   numero++;
+// }
+
+/*Error 1 - Eliminar las llaves... tomaría como sentencia propia del while, son el Console.WriteLine, sin incrementarle la unidad. Por lo que "numero" siempre sería 1, y 1 siempre menor a 100, por lo que no saldría nunca del ciclo
+Error 2 - reemplazar el 100, por un número superior al 255 (el límite de los tipo byte), dado que luego del 255, volvería al 1, nunca llegaría a superar el 256, por lo que se ejecutaría por siempre*/
+
+
+
+/*************************************************************** Ejercicios WHILE Laboratorio Ej. 4
+Imprimir los números del 1 a 10 sin imprimir los números 2,5 y 9 uno abajo del otro*/
+
+// byte number = 1;
+
+// while (number <= 10)
+// {
+//   if (number != 2 && number != 5 && number != 9)
+//     Console.WriteLine(number);
+
+//   number++;
+// }
+
+
+
+/*************************************************************** Ejercicios WHILE Laboratorio Ej. 5
+Imprimir los números del 1 a 30 sin imprimir los números entre el 10 y el 20 uno abajo del otro*/
+
+// byte number = 1;
+
+// while (number <= 30)
+// {
+//   if (number < 10)
+//     Console.WriteLine(number);
+//   if (number > 20)
+//     Console.WriteLine(number);
+
+//   number++;
+// }
+
+
+
+
+/*************************************************************** Ejercicios WHILE Laboratorio Ej. 6
+Imprimir la suma de los números del 1 a 10*/
+
+// short number = 1, suma = 0; // OJO con los límites de los tipos de datos.. acá el short es por la variable suma que llega a 11325
+
+// while (number <= 150)
+// {
+
+//   suma += number++;
+// }
+
+// Console.WriteLine(suma);
+
+
+
+
+/*************************************************************** Ejercicios WHILE Laboratorio Ej. 7
+Imprimir la suma de los números pares del 1 a 25*/
+
+// byte numero = 1;  // Esta forma me parece mejor, así no se deja reservada más memoria para numero, de la que hace falta
+// short sumaPares = 0;
+
+// while (numero <= 25)
+// {
+//   if (numero % 2 == 0)
+//     sumaPares += numero;
+
+//   numero++;
+// }
+// Console.WriteLine(sumaPares);
+
+
+
+/*************************************************************** Ejercicios Laboratorio Adicional 4 - Ej. 1
+Imprimir los nros del 1 al 10 acorde al diagrama de flujo*/
+
+// byte numero = 1;
+
+// while (numero <= 10)
+// {
+//   Console.WriteLine(numero);
+//   numero++;
+// }
+
+
+/*************************************************************** Ejercicios Laboratorio Adicional 4 - Ej. 2
+Imprimir los nros del 1 al 10, salteando de a 2, acorde al diagrama de flujo*/
+
+// int numero = 1;
+
+// while (numero <= 10)
+// {
+//   numero = numero + 2;
+//   Console.WriteLine(numero);
+// }
+
+
+
+/*************************************************************** Ejercicios Laboratorio Adicional 4 - Ej. 3
+Imprimir los nros del 10 al 1, uno debajo del otro, acorde al diagrama de flujo*/
+
+// byte numero = 10;
+
+// while ( numero >= 1)
+// {
+//   Console.WriteLine(numero);
+//   numero--;
+// }
+
+
+
+/*************************************************************** Ejercicios Laboratorio Adicional 4 - Ej. 4
+Imprimir los nros del 1 al 10, sin imprimir 2,5,9, acorde al diagrama de flujo*/
+
+// byte numero = 1;
+
+// while (numero <= 10)
+// {
+//   if (numero != 2 && numero != 5 && numero != 9)
+//   {
+//     Console.WriteLine(numero);
+//   }
+//   numero++;
+// }
+
+
+
+
+/*************************************************************** Ejercicios Laboratorio Adicional 4 - Ej. 5
+Imprimir los nros del 1 al 30, sin imprimir los nros entre 10 y 20, uno debajo del otro, acorde al diagrama de flujo*/
+
+// byte numero = 1;
+
+// while (numero <= 30)
+// {
+//   if (numero <= 10 || numero >= 20)
+//   {
+//     Console.WriteLine(numero);
+//   }
+//   numero++;
+// }
+
+
+
+/*************************************************************** Ejercicios Laboratorio Adicional 4 - Ej. 6
+Imprimir la suma de los nros pares del 1 al 10, acorde al diagrama de flujo*/
+
+// byte numero = 1;
+// short suma = 0;
+
+// while (numero <= 10)
+// {
+//   suma += numero; 
+//   numero++;
+// }
+
+// Console.WriteLine(" Suma = {0}", suma);
+
+
+
+/*************************************************************** Ejercicios Laboratorio Adicional 4 - Ej. 7
+Imprimir la suma de los nros pares del 1 al 25, acorde al diagrama de flujo*/
+
+// byte numero = 1;
+// short suma = 0;
+
+// while (numero <= 25)
+// {
+//   if (numero % 2 == 0)
+//   { suma += numero; }
+
+//   numero++;
+// }
+
+// Console.WriteLine(" Suma = {0}", suma);
+
+
+
+/*************************************************************** Ejercicios Laboratorio Adicional 4 - Ej. 8
+Imprimir la multiplicaciopn de los números impares que se encuentran entre -10 y 10, uno debajo del otro, acorde al diagrama de flujo*/
+
+int producto = 1, numero = -10;
+
+while (numero <= 10)
 {
-  menuSelection = readResult.ToLower();
+  //Console.WriteLine(numero);
+  if (numero % 3 == 0 && numero != 0)
+  {
+    producto *= numero;
+  }
+  numero++;
 }
 
-switch (int.Parse(menuSelection))
-{
-  case 1:
-    Console.WriteLine($"La suma de {numero1} y {numero2} es igual a {numero1 + numero2} ");
-    break;
-  case 2:
-    Console.WriteLine($"La resta de {numero1} y {numero2} es igual a {numero1 - numero2} ");
-    break;
-  case 3:
-    Console.WriteLine($"El producto entre {numero1} y {numero2} es igual a {numero1 * numero2} ");
-    break;
-  case 4:
-    Console.WriteLine($"La división entre {numero1} y {numero2} es igual a {numero1 / numero2} ");
-    break;
-  default:
-    Console.WriteLine("No es una operación válida");
-    break;
-}
+Console.WriteLine(producto);
+
+
