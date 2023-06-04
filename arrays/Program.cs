bf0b52b13db2fdaa2803349e3e359e287258ee3e
AlbertoRanno <albertoranno@hotmail.com>
2023-06-04 01:06:26 -0300

    ult modificacion arrays

--- arrays/Program.cs
+++ arrays/Program.cs
@@ -1,4 +1,47 @@
-﻿/*
+﻿/*************************************** ARRAYS
+Hasta ahora siempre había que saber con antelación el número de variables que el programa iba a necesitar.
+Sin embargo, habrá situaciones en las que no sea posible determinar este número hasta que el programa no se esté ejecutando. Pues bien, para solucionar estas dificultades contamos con las estructuras de arreglos, arrays en inglés. 
+
+/*************************************** Arreglos de una dimensión
+Un array es una variable que es capaz de almacenar varios valores en forma simultánea.
+Cada uno de estos valores se identifica mediante un número que se denomina “índice”.
+Para acceder al primer elemento del array se usa el índice cero, para el segundo el índice uno, para el tercero el índice dos, y así sucesivamente. 
+
+Al arreglo se lo declara de la siguiente forma: tipo[] variable;
+
+Es muy parecido a cómo se declara una variable normal, sólo que hay que poner corchetes detrás del tipo de dato.
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!IMPORTANTE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+En C# los arrays son objetos derivados de la clase System.Array. Por lo tanto, y esto es muy importante, cuando declaramos un array en C# este aún no se habrá creado en la memoria de la computadora, en consecuencia, antes de poder usarlos habrá que instanciarlos, como si fuera cualquier otro objeto.
+
+Veamos un breve ejemplo:
+string[] nombres; // Declaración del array
+nombres = new string[3]; // Instanciación del array
+
+En efecto, el array nombres será utilizable únicamente a partir de su instanciación, y podrá tener un máximo de tres elementos, accedidos desde los índices 0, 1 y 2.
+Nótese que índice del array comienza de CERO (0) y NO de uno (1), por lo cual el índice del último elemento es siempre el TotalDeElementos menos uno (3-1=2)
+
+/*************************************** Operaciones de recorrida de arreglos
+Un array es un conjunto de objetos, ordenado y de tamaño fijo.
+Para acceder a cualquier elemento de este conjunto se aplica el operador postfijo [] sobre la tabla para indicar entre corchetes 
+la posición que ocupa el objeto al que se desea acceder dentro del conjunto. 
+
+Es decir, este operador se usa así: [<posiciónElemento>]
+
+Un ejemplo de su uso, en el que se asigna al elemento que ocupa la posición 3 en una tabla de nombre arrayPrueba, el valor del elemento que ocupa la posición 18 de dicha tabla es el siguiente: 
+
+arrayPrueba[3] = arrayPrueba[18];
+*/
+
+
+
+
+
+
+
+
+/*
 *Una matriz es una variable especial que contiene una secuencia de elementos de datos relacionados.
 *Debe memorizar el formato básico de una declaración de variable de matriz.
 *Acceda a cada elemento de una matriz para establecer u obtener sus valores mediante el uso de un índice de base cero entre corchetes.
