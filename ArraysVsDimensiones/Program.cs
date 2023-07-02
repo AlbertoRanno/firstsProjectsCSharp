fd1a589656db06b95d1a55148769239d747d3a90
AlbertoRanno <albertoranno@hotmail.com>
2023-07-01 21:37:37 -0300

    Arrays vs dimensiones e introduccion a funciones y procedimientos

---
+++ ArraysVsDimensiones/Program.cs
@@ -0,0 +1,187 @@
+﻿/* Un arreglo de dos dimensiones, es una matriz, y los elementos se acceden desde dos coordenadas, una que representa la fila y otra que representa la columna.
+0 -1 -2 -3 -4 -5 -6 -7 -8 -9
+0 1 2 3 4 5 6 7 8 9
+● Esta matriz tiene 2 filas y 10 columnas.
+● Contiene números enteros positivos y negativos.
+● El índice de la fila va desde 0 a 1 (son 2 filas).
+● El índice de la columna va desde 0 a 9 (son 10 columnas).
+ ejemplo:
+Creamos una matriz de 2 filas y 10 columnas, la llenamos con números aleatorios positivos menores a 100 (con el generador de números Random), y a la vez vamos mostrando el valor de cada elemento de la matriz:
+ */
+
+// static void Main(string[] args)
+// {
+// const int MaxFilas = 2;
+// const int MaxColumnas = 10;
+// Random objRandom = new Random();
+// Console.WriteLine("Matriz");
+// int[,] Matriz = new int[MaxFilas, MaxColumnas];
+// for (int i = 0; i < MaxFilas; i++)
+// {
+//   for (int j = 0; j < MaxColumnas; j++)
+//   {
+//     Matriz[i, j] = objRandom.Next(100);
+//     Console.WriteLine($"Matriz[{i},{j}] = {Matriz[i, j]}");
+//   }
+// }
+// Console.ReadKey();
+//}
+
+/*
+Si se imaginara una dimensión más, se tendría un cubo que es un arreglo de tres dimensiones.
+Sus elementos se acceden con tres coordenadas, la primera que representa la fila, la segunda que representa la columna, y una tercera que representa la profundidad dentro del cubo.
+En la imagen de la derecha se ilustra un cubo de 4 filas, 5 columnas y 3 niveles de profundidad.
+Un array de tres dimensiones (4 x 5 x 3)
+ ejemplo:
+Creamos un cubo de 4 filas, 5 columnas y 3 niveles de profundidad, lo llenamos con números aleatorios positivos menores a 100 (con el generador de números Random), y a la vez vamos mostrando el valor de cada elemento del cubo:
+*/
+// static void Main(string[] args)
+// {
+//   const int MaxFilas = 4;
+//   const int MaxColumnas = 5;
+//   const int MaxNiveles = 3;
+//   Random objRandom = new Random();
+//   Console.WriteLine("Cubo" + Environment.NewLine);
+//   int[,,] Cubo = new int[MaxFilas, MaxColumnas, MaxNiveles];
+//   for (int i = 0; i < MaxFilas; i++)
+//   {
+//     for (int j = 0; j < MaxColumnas; j++)
+//     {
+//       for (int k = 0; k < MaxNiveles; k++)
+// {
+//         Cubo[i, j, k] = objRandom.Next(100);
+//         Console.WriteLine($"Cubo [{i},{j},{k}] = {Cubo[i, j, k]}");
+//       }
+//     }
+//   }
+// Console.ReadKey();
+// }
+
+/*Nombre del arrego, si lo llamo con console.WriteLine, no voy a ver lo tengo, sus valores en cada coordenada... no.
+Simplemente voy a ver el tipo de dato, esto es porque en el nombre se almacena la direccion de memoria. Una Referencia.
+Por eso si quiero los valores del arreglo, tengo que trabajar con el valor del arreglo + sus subindices.
+
+Para imprimir todos los datos de un arreglo, tendre que imprimir variable por variable. Sirve el FOR.
+
+Para el largo NombreArreglo.Length
+
+ */
+
+// Console.WriteLine(Cubo.Length); // En varias dimensiones, dará el largo total, en este caso: 60 = (4 x 5 x 3)
+
+/* Si quisiera acceder a un indice, que excede al maximo que tiene mi arreglo, estaria queriendo acceder a memoria que no tengo accedo por lo que da error en tiempo de ejecucion 
+
+Copia de arreglos variable a variable mediante un for:
+*/
+// char[] origen = { '@', 'q', 'D', 'T', 'l' };
+// char[] destino = new char[5];
+// for (int i = 0; i < origen.Length; i++)
+// {
+//   destino[i] = origen[i]; //Ojo con el orden
+// }
+// for (int i = 0; i < destino.Length; i++)
+// {
+//   Console.WriteLine(destino[i]);
+// }
+
+/*Promedio*/
+// short[] promedio = { -2344, 453, -2355, 666, 550, 3555 };
+// int suma = 0;
+
+// for (int i = 0; i < promedio.Length; i++)
+// {
+//   suma += promedio[i];
+// }
+
+// Console.WriteLine($"El promedio es {suma/(promedio.Length)}");
+
+/*El Mayor y El Menor*/
+// short elMayor = promedio[0], elMenor = promedio[0];
+// for (int i = 1; i < promedio.Length; i++)
+// {
+//   if (elMayor < promedio[i])
+//   {
+//     elMayor = promedio[i];
+//   }
+//   if (elMenor > promedio[i])
+//   {
+//     elMenor = promedio[i];
+//   }
+// }
+// Console.WriteLine($"El mayor {elMayor} El menor {elMenor}");
+
+// Inicialización Array 2 dimensiones, y forma copada para visualizarlo:
+// byte[,] array2D = {   {3,123,99},
+//                       {23,54,99},
+//                       {76,77,99},
+//                       {40,36,99},
+//                       {35,67,99},
+//                       {76,21,99}
+//                       };
+// for (int i = 0; i < array2D.GetLength(0); i++) //GetLength da la longitud de las filas (indice 0)
+// {
+//   for (int j = 0; j < array2D.GetLength(1); j++) //GetLength da la longitud de las columnas (indice 1)
+//   {
+//     Console.Write($"{array2D[i, j]}\t"); //Write para uno al lado del otro, espaciado con tabulador
+//   }
+//   Console.WriteLine(); //El cambio de linea cada vez que termina de recorrer las columnas
+// }
+
+/* EJ 1
+Uso de un arreglo para ingreso de datos por teclado.
+Ingrese por teclado la facturación de los últimos 6 meses (se ingresa un importe por cada mes).
+Solo se pueden ingresar números.
+Informar
+1. La facturación total (sumatoria).
+2. El promedio de facturación de los últimos 6 meses.
+3. El valor máximo facturado.
+4. El valor mínimo facturado.
+*/
+// string? ReadResult;
+// double[] facturacionXMes = new double[6];
+// byte cantMeses = 6;
+// double facturacionTotal = 0, elMenorMonto = facturacionXMes[0], elMayorMonto = facturacionXMes[0];
+
+// Console.Clear();
+// Console.WriteLine("Bienvenido a la app!\n");
+// for (int i = 0; i < cantMeses; i++)
+// {
+//   Console.Write($"Ingresá la facturación del Mes {i + 1}: ");
+//   ReadResult = Console.ReadLine();
+//   if (ReadResult != null)
+//   {
+//     facturacionXMes[i] = double.Parse(ReadResult);
+//     facturacionTotal += facturacionXMes[i];
+//   }
+
+//   if (elMayorMonto < facturacionXMes[i])
+//   {
+//     elMayorMonto = facturacionXMes[i];
+//   }
+//   if (elMenorMonto > facturacionXMes[i])
+//   {
+//     elMenorMonto = facturacionXMes[i];
+//   }
+// }
+// for (int i = 0; i < cantMeses; i++)
+// {
+//   Console.WriteLine($"La facturación del mes {i + 1}, fue de ${facturacionXMes[i]}");
+// }
+// Console.WriteLine($"La facturación total del mes fue de {facturacionTotal}");
+// Console.WriteLine($"El promedio de la facturación fue de {facturacionTotal / cantMeses}");
+// Console.WriteLine($"El mes que menos se facturó {elMenorMonto}");
+// Console.WriteLine($"El mes que más se facturó {elMayorMonto}");
+
+/* EJ 2
+Copiar el contenido del arreglo origen al arreglo destino utilizando una estructura de control de flujo repetitiva y, luego, informar el índice y los valores del arreglo destino.
+*/
+char[] origen = { '@', 'q', 'D', 'T', 'l' };
+char[] destino = new char[5];
+for (int i = 0; i < origen.Length; i++)
+{
+  destino[i] = origen[i];
+}
+for (int i = 0; i < destino.Length; i++)
+{
+  Console.WriteLine($"En la posición origen{i} {destino[i]}");
+}
---
+++ FuncionesYProcedimientos/Program.cs
@@ -0,0 +1,161 @@
+﻿/*
+Un problema complejo se puede dividir en pequeños subproblemas más sencillos. Estos subproblemas se conocen como módulos y su implementación en un lenguaje de programación se llama “subprogramas”.
+Existe una clasificación de subprograma: los procedimientos y las funciones. Veremos usos y diferencias de cada uno.
+Un subprograma realiza las mismas acciones que un programa, sin embargo, se utilizará el subprograma o módulo para una acción u operación específica.
+Un subprograma recibe datos de un programa y éste le devuelve resultados (el programa “llama” o “invoca” al subprograma, éste ejecuta una tarea específica formada por un conjunto de instrucciones y devuelve el “control” al programa que lo llamó).
+En C#, y en la programación orientada a objetos (POO), las funciones y procedimientos se conocen con el nombre de “métodos” y son subprogramas. 
+
+Funciones (retornan un valor)
+En el ámbito de la programación, una función es un subprograma que contiene una secuencia de instrucciones que realizan una tarea específica dentro de un programa o aplicación más grande. Las declaraciones de las funciones generalmente son especificadas por:
+● Un nombre único en el ámbito o alcance:
+nombre de la función con el que se la identifica y se la distingue de otras. No podrá haber otra función o procedimiento con ese nombre (salvo en sobrecarga o polimorfismo, una de las características de la Programación Orientada a Objetos, o POO).
+● Un tipo de dato de retorno: tipo de dato del valor que la función devolverá al terminar su ejecución.
+● Una lista de parámetros: especificación del conjunto de parámetros (pueden ser cero, uno o más) que la función debe recibir para realizar su tarea.
+
+Sintaxis de una Función
+Ejemplo:
+ModificadorDeAcceso TipoDeDatoDevuelto Nombre_Funcion(tipo(s) parámetros(s) nombres)
+{
+// declaración de datos y cuerpo de la función.
+return (valor);
+}
+
+static int Funcion_Sumar(int numero1, int numero2)
+ {
+int resultado = numero1 + numero2;
+return resultado;
+ }
+
+ Procedimientos (no retornan valor)
+Un procedimiento es un subprograma que realiza una tarea específica y es relativamente independiente del resto del código de la aplicación.
+Los procedimientos suelen utilizarse para reducir la duplicación de código en un programa. Los procedimientos pueden recibir parámetros, pero no necesitan devolver un valor como es el caso de las funciones.
+
+Sintaxis Procedimiento
+Ejemplo:
+ModificadorDeAcceso void Nombre_Procedimiento(tipo(s) parámetros(s) nombres)
+{
+// declaración de datos y cuerpo del procedimiento.
+}
+static void Procedimiento_Sumar(int numero1, int numero2)
+ {
+int resultado = numero1 + numero2;
+Console.WriteLine(resultado);
+ }
+
+ Parámetros
+Un parámetro es un dato que ingresa a una función o procedimiento a través de su llamada.
+Todos los tipos de dato de C#, son tipos de valor o tipos de referencia. De forma predeterminada, los tipos de valor y los tipos de referencia se pasan a un procedimiento o función por valor.
+Asociando con lo visto en clase, un arreglo es un tipo de referencia dado que es necesario instanciarlo con la palabra reservada new. Es decir, es un objeto.
+
+Pasar parámetros por valor
+Cuando un tipo de valor se pasa a un método por valor, se pasa una copia del objeto y no el propio objeto. Por lo tanto, los cambios realizados en el objeto en el método llamado no tienen ningún efecto en el objeto original cuando el control vuelve al autor de la llamada.
+En el ejemplo siguiente se pasa un tipo de valor a un método por valor, y el método llamado intenta cambiar el valor del tipo de valor. Define una variable de tipo int, que es un tipo de valor, inicializa su valor en 20 y lo pasa a un método denominado ModificarValor que cambia el valor de la variable a 30. Pero cuando el método vuelve, el valor de la variable no cambia.
+*/
+
+// using System;
+// using System.Collections.Generic;
+// using System.Linq;
+// using System.Text;
+// using System.Threading.Tasks;
+// namespace Metodos
+// {
+//   class MetodoFuncion
+//   {
+//     static void Main(string[] args)
+//     {
+//       int valor = 20;
+//       Console.WriteLine("En el Main(), valor tiene = {0}", valor);
+//       ModificarValor(valor);
+//       Console.WriteLine("Volviendo al Main, valor tiene = {0}", valor);
+//       Console.ReadKey();
+//     }
+
+// static void ModificarValor(int i)
+//     {
+//       i = 30;
+//       Console.WriteLine($"En ModificarValor(), el valor del parámetro i es = {i}");
+//       return;
+//     }
+//   }
+// }
+/* Esto imprime:
+En el Main(), valor tiene = 20
+En ModificarValor(), el valor del parámetro i es = 30
+Volviendo al Main, valor tiene = 20
+
+Cuando un objeto de un tipo de referencia se pasa a un método por valor, se pasa por valor una referencia al objeto. Es decir, el método no recibe el objeto concreto, sino un argumento que indica la ubicación en memoria del objeto (una dirección de memoria).
+Si cambia un miembro del objeto mediante esta referencia, el cambio se reflejará en el objeto cuando el control vuelva al método de llamada.
+Pero el reemplazo del objeto pasado al método no tendrá ningún efecto en el objeto original cuando el control vuelva al autor de la llamada.
+En el ejemplo del siguiente slide se define una clase (que es un tipo de referencia) denominada EjemploRefType.
+Crea una instancia de un objeto EjemploRefType, asigna 44 a su campo value y pasa el objeto al método ModificarObjeto. Fundamentalmente, este ejemplo hace lo mismo que el ejemplo anterior: pasa un argumento por valor a un método. Pero, debido a que se usa un tipo de referencia, el resultado es diferente.
+La modificación que se lleva a cabo en ModificarObjeto para el campo obj.value cambia también el campo value del argumento rt, en el método Main a 33, tal y como muestra el resultado del ejemplo. */
+
+// using System;
+// using System.Collections.Generic;
+// using System.Linq;
+// using System.Text;
+// using System.Threading.Tasks;
+// namespace Metodos
+// {
+//   class MetodoFuncion
+//   {
+//     static void Main(string[] args)
+//     {
+//       var rt = new EjemploRefType();
+//       rt.value = 44;
+//       Console.WriteLine("En el Main(), el valor del objeto rt es = {0}", rt.value);
+//       ModificarObjeto(rt);
+//       Console.WriteLine("Y al volver del procedimiento ModificarObjeto es = {0}", rt.value);
+//       Console.ReadKey();
+//     }
+//     static void ModificarObjeto(EjemploRefType obj)
+//     {
+//       obj.value = 33;
+//     }
+//   }
+//   public class EjemploRefType
+//   {
+//     public int value;
+//   }
+// }
+/* Esto imprime:
+En el Main(), el valor del objeto rt es = 44
+Y al volver del procedimiento ModificarObjeto es = 33
+ 
+ Pasar parámetros por referencia
+Se pasa un parámetro por referencia cuando se quiere cambiar el valor de un argumento en un método, y reflejar ese cambio cuando el control vuelva al método de llamada. Para pasar un parámetro por referencia, se usa la palabra clave ref o out. También se puede pasar un valor por referencia para evitar la copia e impedir modificaciones usando la palabra clave in.
+El ejemplo siguiente es idéntico al anterior, salvo que el valor se pasa por referencia al método ModificarValor. Al modificar el valor del parámetro en el método ModificarValor, el cambio del valor se refleja cuando el control vuelve al autor de la llamada. */
+
+// using System;
+// using System.Collections.Generic;
+// using System.Linq;
+// using System.Text;
+// using System.Threading.Tasks;
+// namespace Metodos
+// {
+// class MetodoFuncion
+//  {
+// static void Main(string[] args)
+//  {
+// int valor = 20;
+// Console.WriteLine("En el Main(), valor tiene = {0}", valor);
+// ModificarValor(ref valor);
+// Console.WriteLine("Volviendo al Main, valor tiene = {0}", valor);
+// Console.ReadKey();
+//  }
+// Static void ModificarValor(ref int i)
+//  {
+//  i = 30;
+// Console.WriteLine("En ModificarValor(), el valor del parámetro i es = {0}", i);
+// return;
+//  }
+//  }
+// }
+
+/*
+Importante: POO
+Se recomienda que antes de tomar el siguiente curso de C#, realices el curso de Paradigma de Objetos.
+
+Desde aquí los ejercicios siguientes están en VS
+*/
+
